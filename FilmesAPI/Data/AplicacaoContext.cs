4854614a3f7b821bb984b40924873792443f7f49
tomygoldboimel <tomyboimel5@gmail.com>
2025-10-05 23:37:31 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Data/AplicacaoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmpregaAPI.Models;
+
+namespace EmpregaAPI.Data
+{
+    public class AplicacaoContext : DbContext
+    {
+        public AplicacaoContext(DbContextOptions<AplicacaoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Curriculo> Curriculos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; } 
+    }
+}
