6f55314d72a7eae932115b8eda2117e7688ebdf0
tomygoldboimel <tomyboimel5@gmail.com>
2025-10-28 22:23:42 -0300

    Migration

---
+++ EmpregaAPI/Migrations/20251029012234_AdicionarIndiceUnicoNoEmail.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmpregaAI.Migrations
+{
+    public partial class AdicionarIndiceUnicoNoEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_Email",
+                table: "Usuarios",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Usuarios_Email",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
